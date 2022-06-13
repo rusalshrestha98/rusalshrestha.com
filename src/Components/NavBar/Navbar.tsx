b8434c294243a60a79d1669bb59a4aa847f30cf6
Rusal Shrestha <rusalo101@gmail.com>
2022-06-13 00:49:20 -0400

    small changes

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -2,7 +2,17 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Rusal</div>
+    <div>
+      <h1>Rusal Shrestha</h1>
+      <nav>
+        <a href="/html/">Home</a>
+        <a href="/css/">Video</a>
+        <a href="/js/">Photo</a>
+        <a href="/python/">Web Dev</a>
+        <a href="/python/">Blog</a>
+        <a href="/python/">Contact</a>
+      </nav>
+    </div>
   );
 };
 
